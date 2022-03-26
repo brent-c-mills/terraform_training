86b3f5d4508ed24e4459a53fa5c1c7a230366201
Brent Mills <brent.c.mills@gmail.com>
2022-03-26 17:34:17 -0400

    Set this up for module calls

--- projectfile.tf
+++ projectfile.tf
@@ -1,24 +1,11 @@
 # unique implementation declarations
 
-# random ID generation
-resource "random_id" "instance_id_0001" {
-  byte_length = 8
-}
-
-# Generate a compute engine instance
-resource "google_compute_instance" "default_instance" {
-  name         = "${var.compute_name_stem}-${random_id.instance_id_0001.hex}"
-  machine_type = "${var.compute_type}"
-  zone         = "${var.gcp_zone}"
-
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-stream-8"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-
+module "my_first_compute" {
+  source            = "git@github.com:kaizoku0506/terraform-module-training.git?ref=v0.1.0"
+  gcp_project       = "argon-potential-319121"
+  gcp_creds_file    = "argon-potential-319121-db2b2157cd94.json"
+  gcp_region        = "us-east1"
+  gcp_zone          = "us-east1-b"
+  compute_name_stem = "brent-compute"
+  compute_type      = "e2-micro"
 }
