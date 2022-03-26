8314c89164691c039adb0b7d4925c1c57edb50aa
Abigail Groff <abigailgroff@Abigail-Groff-MBP13-2020.localdomain>
2022-03-26 17:58:00 -0400

    getting ready to run

--- projects/abbie-compute-1/projectfile.tf
+++ projects/abbie-compute-1/projectfile.tf
@@ -2,10 +2,10 @@
 
 module "my_first_compute" {
   source            = "git@github.com:kaizoku0506/terraform-module-training.git?ref=v0.1.0"
-  gcp_project       = "argon-potential-319121"
-  gcp_creds_file    = "argon-potential-319121-db2b2157cd94.json"
+  gcp_project       = "verdant-cargo-345213"
+  gcp_creds_file    = "verdant-cargo-345213-b9088e0a4aca.json"
   gcp_region        = "us-east1"
   gcp_zone          = "us-east1-b"
-  compute_name_stem = "brent-compute"
+  compute_name_stem = "abbie-compute"
   compute_type      = "e2-micro"
 }
